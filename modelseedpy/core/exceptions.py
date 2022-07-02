404c375538d177487ca9e0729d64a0c67e785a38
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-07-02 17:27:19 -0400

    initial commit

---
+++ modelseedpy/core/exceptions.py
@@ -0,0 +1,22 @@
+
+#Adding a few exception classes to handle different types of errors in a central file
+class FeasibilityError(Exception):
+    """Error in FBA formulation"""
+    def __init__(self, message):
+        super(FeasibilityError, self).__init__(message)
+        
+class PackageError(Exception):
+    """Error in package manager"""
+    pass
+
+class GapfillingError(Exception):
+    """Error in model gapfilling"""
+    pass
+
+class ObjectError(Exception):
+    """Error in the construction of a base KBase object"""
+    pass 
+
+class SubOptimalError(Exception):
+    'Error in Optlang model optimization'
+    pass
