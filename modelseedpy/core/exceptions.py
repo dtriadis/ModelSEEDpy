cfe9f756147e67f9f143cfe2a6ce721d1bd6782f
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-07-02 16:56:39 -0400

    initial commit

--- modelseedpy/core/exceptions.py
+++ modelseedpy/core/exceptions.py
@@ -8,3 +8,7 @@ class FeasibilityError(Exception):
 class PackageError(Exception):
     """Error in package manager"""
     pass
+
+class ObjectError(Exception):
+    """Error in the construction of a base KBase object"""
+    pass
--- modelseedpy/core/msgenomeclassifier.py
+++ modelseedpy/core/msgenomeclassifier.py
@@ -4,16 +4,15 @@ from modelseedpy.ml.predict_phenotype import create_indicator_matrix
 class MSGenomeClassifier:
 
     def __init__(self, model, model_features):
-        self.features = model_features
-        self.model = model
+        self.model = model; self.features = model_features
 
     @staticmethod
     def extract_features_from_genome(genome, ontology_term):
         """
-
+        
         :param genome: ModelSEED Genome to classify
         :param ontology_term: Ontology Term to classify (Example: RAST)
-        :return:
+        :return: 
         """
         features = set()
         for feature in genome.features:
