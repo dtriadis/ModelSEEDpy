2e7f9991a4f54e4fb7993a9f355fad59cac6f182
Andrew Freiburger <freiburgermsu@aol.com>
2023-08-23 00:32:34 -0500

    CommScores upates and associated edits

--- modelseedpy/__init__.py
+++ modelseedpy/__init__.py
@@ -40,7 +40,7 @@ from modelseedpy.core import (
 from modelseedpy.core.exceptions import *
 
 from modelseedpy.community import (
-    MSCommunity, CommunityMembers, MSCompatibility, MSKineticsFBA, CommScores, MSCommPhitting,
+    MSCommunity, CommunityMember, MSCompatibility, MSKineticsFBA, CommScores, MSCommPhitting,
     MSSteadyCom, build_from_species_models, phenotypes)
 
 from modelseedpy.biochem import ModelSEEDBiochem
