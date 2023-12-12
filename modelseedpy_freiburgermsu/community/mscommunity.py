66206b0143aa29c23441ad18fb7f524973afaa69
Andrew Freiburger <freiburgermsu@aol.com>
2023-12-11 20:46:49 -0600

    circular import scrubbing

--- modelseedpy_freiburgermsu/community/mscommunity.py
+++ modelseedpy_freiburgermsu/community/mscommunity.py
@@ -115,8 +115,7 @@ class MSCommunity:
                         if printing:  print('primary biomass defined', rxn.id)
                         self.primary_biomass = rxn
                     elif rxn.metabolites[self.biomass_cpd] < 0 and len(rxn.metabolites) == 1:  self.biomass_drain = rxn
-            elif 'c' in self.biomass_cpd.compartment:
-                other_biomass_cpds.append(self.biomass_cpd)
+            elif 'c' in self.biomass_cpd.compartment:    other_biomass_cpds.append(self.biomass_cpd)
         # assign community members and their abundances
         abundances = abundances or [1/len(other_biomass_cpds)]*len(other_biomass_cpds)
         self.members = DictList(
